de0f593be96be2a3ef10cdabeaea94e1cced4979
Roberto <robertodermato@gmail.com>
2019-09-20 11:44:21 -0300

    fatorial

---
+++ src/AppRecursivos.java
@@ -0,0 +1,16 @@
+public class AppRecursivos {
+
+    public static void main (String args[]){
+
+        System.out.println("Testando métodos recursivos");
+
+        System.out.println("Fatorial");
+        System.out.println("Fatorial de 0: " + RecursivosNumeros.fatorial(0));
+        System.out.println("Fatorial de 1: " + RecursivosNumeros.fatorial(1));
+        System.out.println("Fatorial de 2: " + RecursivosNumeros.fatorial(2));
+        System.out.println("Fatorial de 3: " + RecursivosNumeros.fatorial(3));
+        System.out.println("Fatorial de 4: " + RecursivosNumeros.fatorial(4));
+
+    }
+
+}
---
+++ src/RecursivosNumeros.java
@@ -0,0 +1,10 @@
+public class RecursivosNumeros {
+
+    public static double fatorial (int n){
+        if (n==0) return 1;
+        return fatorial(n-1)*n;
+    }
+
+
+
+}
